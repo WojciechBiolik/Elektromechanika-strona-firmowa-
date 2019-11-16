c3f1fc0046fccbd04fe68bd5657284afe4174a5a
WojciechBiolik <wbiolik@gmail.com>
2019-11-16 18:56:40 +0100

    background, deleting files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('.about__paragraph--js');
+console.log("Helo");
+console.log(header);
+header.innerHTML = "hahahahah";
+console.log(header);
+console.log(header.innerHTML);
--- main.js
+++
@@ -1,3 +0,0 @@
-const header = document.querySelector('.footer__copyright');
-console.log("Helo");
-console.log(header.innerHTML);
